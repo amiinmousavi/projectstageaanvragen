3fdc201883e933cd63cc20e154e8dd09d90da6f5
amin mousavi <amin.mousavikharkani@student.vives.be>
2022-05-01 20:08:32 +0200

    initial commit

---
+++ StageAanvragen.Models/Entities/Bedrijf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StageAanvragen.Models.Entities
+{
+    public partial class Bedrijf
+    {
+        public int? Id { get; set; }
+        public string? Naam { get; set; }
+        public string? Adres { get; set; }
+        public string? Stad { get; set; }
+        public string? Website { get; set; }
+        public string? Logo { get; set; }
+    }
+}
