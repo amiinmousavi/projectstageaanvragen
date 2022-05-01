773b099ea525d8f55f42184fc3c1218eaa24fc7d
amin mousavi <amin.mousavikharkani@student.vives.be>
2022-05-01 21:45:31 +0200

    commit

---
+++ StageProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StageProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
